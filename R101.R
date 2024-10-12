77687eb7630d9edf50a9ee5708a68e2a2a41286d
izudata <izumchy@gmail.com>
2024-10-12 10:17:14 -0600

    Body mass index script

--- R101.R
+++ R101.R
@@ -16,3 +16,18 @@ myname$ages
 myname[1,1]
 myname[1,2]
 myname[1, ]
+
+data()
+View(women)
+
+library(tidyverse)
+
+women %>%
+  filter(height > 60 & weight < 130) %>% 
+  mutate(body_mass_index = weight/((height/100)^2)) %>% 
+  select(weight, body_mass_index) %>% 
+  arrange(body_mass_index) %>% 
+  #View()
+  plot()
+
+glimpse(women)
